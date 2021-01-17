d6978089c6c63dee2eb5709c78bf212dd2e2a658
Herve Desrosiers <hdesrosiers@stingray.com>
2021-01-17 11:29:46 -0500

    finished tutorial

--- app/src/main/java/com/hdesrosiers/googlecomposetutorial/MainActivity.kt
+++ app/src/main/java/com/hdesrosiers/googlecomposetutorial/MainActivity.kt
@@ -2,37 +2,75 @@ package com.hdesrosiers.googlecomposetutorial
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
-import com.hdesrosiers.googlecomposetutorial.ui.GoogleComposeTutorialTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GoogleComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Column(modifier = Modifier.fillMaxSize()) {
+                TopAppBar(title = {
+                    Text("Compose Tutorial")
+                })
+                NewsStory()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    val image = imageResource(id = R.drawable.header)
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            val imageModifier = Modifier
+                .preferredHeight(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp))
+            Image(
+                asset = image,
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+            Text(
+                text = "A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = typography.h6)
+            Text(
+                text = "Davenport, California",
+                style = typography.body2)
+            Text(
+                text = "December 2018",
+                style = typography.body2)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DefaultPreview() {
-    GoogleComposeTutorialTheme {
-        Greeting("Android")
-    }
+    NewsStory()
 }
